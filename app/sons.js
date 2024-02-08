d6184d63b2ce7c98d12b655abe140e9916a6798a
Thales Martins <99058389+thalesgmartins@users.noreply.github.com>
2024-02-08 20:24:03 -0300

    .

--- app/sons.js
+++ app/sons.js
@@ -1,5 +1,5 @@
 const naoConsegue = new Audio('./sounds/nao-consegue-ne.mp3');
 const queIssoMeuFilho = new Audio('./sounds/que-e-isso-meu-filho-calma.mp3');
-const pare = new Audio('/.sounds/pare.mp3');
+const pare = new Audio('./sounds/pare.mp3');
 const atumalaka = new Audio('./sounds/atumalaka.mp3');
 const cavalo = new Audio('./sounds/cavalo.mp3');
